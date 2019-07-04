219510f8b444dc130dae15222438042c32595dba
github username <github email address>
2019-07-04 15:42:24 -0400

    format update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
 import manageUsers from './reducers/manageUsers'
-
 
 import { createStore} from 'redux';
 import { Provider } from "react-redux";
+
 
 const store = createStore(manageUsers)
 
