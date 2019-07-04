90dc22139e411c3a18cd26159ea6ef1251b6c5f4
github username <github email address>
2019-07-04 15:41:50 -0400

    import connect and add mapStateProps function for Users.js component.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Users extends Component {
 
   render() {
+    let users = this.props.users.map(user => <li>{user.username}</li>)
     return (
       <div>
+        <h4>User Count: {this.props.userCount}</h4>
         <ul>
-          Users!
+          {users}
         </ul>
       </div>
     )
   }
 }
 
-//add mapStateToProps here
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+    userCount: state.users.length
+  }
+}
 
-export default Users
+
+export default connect(mapStateToProps)(Users);
+// export default Users
