fdd8dd0de966c7357e9895db659c9bd3c883adba
Carolyn Whelpley <carolynwhelpley@Carolyns-Air.fios-router.home>
2019-08-26 12:59:23 -0400

    Interim checkin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import { createStore } from 'redux';
 import manageUsers from './reducers/manageUsers'
-
-
+import { Provider } from 'react-redux';
+const store = createStore(
+  manageUsers
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_()
+);
 // add imports and code
 
 
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
+  <Provider store={store}>
+  <App store={store}/>
+  </Provider>
   ,
   document.getElementById('root')
 );
